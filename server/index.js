f581bbc5d63f54a339e08e5d8caacf765940383a
Faraz Ahmed <masterdot707@gmail.com>
2022-09-17 11:04:24 +0500

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "20mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "20mb", extended: true }));
+
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://faraz12:faraz12@cluster0.gw6bytj.mongodb.net/?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`server running at ${PORT}`)))
+  .catch((err) => console.log(err.message));
